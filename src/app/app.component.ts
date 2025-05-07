137d8e421694a57b2d566047301d55c1cabfae2d
adrianonobre <adrianonobre@mpf.mp.br>
2025-05-07 15:18:35 -0300

    Aula 02 - parte 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { HomePortalComponent } from "./home-portal/home-portal.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    HeaderComponent,
+    HomePortalComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
