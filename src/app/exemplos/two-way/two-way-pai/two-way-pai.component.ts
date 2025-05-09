b77f1877ed4acdb60b3428bcff57d568068942ca
adrianonobre <adrianonobre@mpf.mp.br>
2025-05-09 16:42:31 -0300

    ng-content 1

--- src/app/exemplos/two-way/two-way-pai/two-way-pai.component.ts
+++ src/app/exemplos/two-way/two-way-pai/two-way-pai.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { TwoWayFilhoComponent } from "../two-way-filho/two-way-filho.component";
+import { CardComponent } from '../../../layout/card/card.component';
 
 @Component({
   selector: 'app-two-way-pai',
-  imports: [TwoWayFilhoComponent],
+  imports: [
+    CardComponent,
+    TwoWayFilhoComponent
+  ],
   templateUrl: './two-way-pai.component.html',
   styleUrl: './two-way-pai.component.scss'
 })
