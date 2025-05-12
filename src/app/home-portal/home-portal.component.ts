78f72e4b01e79ec39925b6b14a3192d3014db315
adrianonobre <adrianonobre@mpf.mp.br>
2025-05-12 16:04:46 -0300

    Atalhos do home portal carregados

--- src/app/home-portal/home-portal.component.ts
+++ src/app/home-portal/home-portal.component.ts
@@ -1,8 +1,9 @@
-import { Component, input, TemplateRef } from '@angular/core';
+import { Component, inject, input, TemplateRef } from '@angular/core';
 import { AtalhoSistema } from './atalho-sistema';
 import { PortalDestaquesComponent } from "./portal-destaques/portal-destaques.component";
 import { PortalMaisSistemasComponent } from './portal-mais-sistemas/portal-mais-sistemas.component';
 import { OrdenarPipe } from '../shared/pipes/ordenar.pipe';
+import { HomePortalService } from './home-portal.service';
 
 @Component({
   selector: 'app-home-portal',
@@ -20,34 +21,25 @@ export class HomePortalComponent {
    * O contexto contém um atributo 'atalho' do tipo `AtalhoSistema`, com dados para montar o link. */
   templateLinkMaisSistemas = input<TemplateRef<unknown>>();
 
+  private portalService = inject(HomePortalService);
 
   // Esse array seria obtido como um JSON de resposta a uma requisição, por exemplo
-  private atalhos: AtalhoSistema[] = [
-    { url: 'https://novoportal.mpf.mp.br/novaintra', nome: 'Intranet', icone: 'Intranet-MPF.jpg', destaque: true },
-    { url: 'https://pontodigital.mpf.mp.br/pontodigital', nome: 'Ponto Digital', icone: 'PontoDigital.jpg', destaque: true },
-    { url: 'https://unico.mpf.mp.br/unico', nome: 'Único', icone: 'Unico.jpg', destaque: true },
-    { url: 'https://novoportal.mpf.mp.br/snp', nome: 'SNP', icone: 'SNP.jpg', destaque: true },
-    { url: 'https://novoportal.mpf.mp.br/kairos', nome: 'Kairos', icone: 'Kairos.jpg', destaque: true },
-    { url: 'https://horus.mpf.mp.br/horus', nome: 'Hórus', icone: 'Horus.jpg', destaque: true },
-    { url: 'https://mail.google.com/a/mpf.mp.br', nome: 'Correio Eletrônico', icone: 'Correio.jpg', destaque: true },
-    { url: 'https://novoportal.mpf.mp.br/biblioteca', nome: 'Biblioteca', icone: 'Biblioteca.jpg', destaque: true },
-    { url: 'https://novoportal.mpf.mp.br/apex/f?p=pin', nome: 'PIN', destaque: false },
-    { url: 'https://novoportal.mpf.mp.br/apex/f?p=sigov', nome: 'SIGOV', destaque: false },
-    { url: 'https://radar.mpf.mp.br/radar2', nome: 'RADAR', destaque: false },
-    { url: 'https://novoportal.mpf.mp.br/sisam/portal', nome: 'SISAM', destaque: false },
-    { url: 'https://novoportal.mpf.mp.br/extractus', nome: 'Extractus', destaque: false },
-    { url: 'https://novoportal.mpf.mp.br/sspr', nome: 'Portal de Senhas', destaque: false },
-    { url: 'https://novoportal.mpf.mp.br/autoriza', nome: 'Sistema Autoriza', destaque: false },
-  ];
-
+  private atalhos: AtalhoSistema[] = [];
   protected atalhosDestaque: AtalhoSistema[] = [];
   protected atalhosMaisSistemas: AtalhoSistema[] = [];
 
 
-  constructor() {
-    this.atualizarListasAtalhos();
+  ngOnInit() {
+    this.carregarAtalhos();
   }
 
+  protected carregarAtalhos() {
+    this.portalService.carregarAtalhos()
+      .then((atalhos) => {
+        this.atalhos = atalhos;
+        this.atualizarListasAtalhos();
+      });
+  }
 
   protected promoverAtalhoMaisSistemas(atalho: AtalhoSistema) {
     this.atalhos = this.atalhos.map(a => a.url === atalho.url ? {...a, destaque: true} : a);
