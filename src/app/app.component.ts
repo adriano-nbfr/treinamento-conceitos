2bad6f031358551282a47c99806e174cf0df6093
adrianonobre <adrianonobre@mpf.mp.br>
2025-05-12 15:13:01 -0300

    Aula 04 - parte 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { HomePortalComponent } from "./home-portal/home-portal.component";
 import { LayoutComponent } from './layout/layout.component';
 import { TwoWayPaiComponent } from "./exemplos/two-way/two-way-pai/two-way-pai.component";
+import { UsuariosListagemComponent } from "./exemplos/usuarios/usuarios-listagem/usuarios-listagem.component";
 
 @Component({
   selector: 'app-root',
   imports: [
     LayoutComponent,
-    HomePortalComponent,
-    // TwoWayPaiComponent
+    // HomePortalComponent,
+    UsuariosListagemComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
