f7b7e565f6fa6b2d8c3d46962b8121b333a81062
adrianonobre <adrianonobre@mpf.mp.br>
2025-05-14 15:30:44 -0300

    Rotas de primeiro nível

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
-import { HomePortalComponent } from "./home-portal/home-portal.component";
+import { RouterOutlet } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
-import { TwoWayPaiComponent } from "./exemplos/two-way/two-way-pai/two-way-pai.component";
-import { UsuariosListagemComponent } from "./exemplos/usuarios/usuarios-listagem/usuarios-listagem.component";
 
 @Component({
   selector: 'app-root',
   imports: [
     LayoutComponent,
-    // HomePortalComponent,
-    UsuariosListagemComponent
-],
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomePortalComponent } from './home-portal/home-portal.component';
+import { UsuariosListagemComponent } from './exemplos/usuarios/usuarios-listagem/usuarios-listagem.component';
+import { TwoWayPaiComponent } from './exemplos/two-way/two-way-pai/two-way-pai.component';
+import { ErroNavegacaoComponent } from './erro-navegacao/erro-navegacao.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'portal',
+    component: HomePortalComponent
+  },
+  {
+    path: 'exemplos/usuarios',
+    component: UsuariosListagemComponent
+  },
+  {
+    path: 'exemplos/two-way',
+    component: TwoWayPaiComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'portal'
+  },
+  {
+    path: '**',
+    component: ErroNavegacaoComponent
+  }
+];
