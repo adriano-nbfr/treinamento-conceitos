2517979436e5910662d9ffdf012b6b0165b60595
adrianonobre <adrianonobre@mpf.mp.br>
2025-05-14 17:40:15 -0300

    Aula 05

--- src/app/exemplos/usuarios/usuario-visualizacao/resolve.ts
+++ src/app/exemplos/usuarios/usuario-visualizacao/resolve.ts
@@ -3,6 +3,6 @@ import { ActivatedRouteSnapshot, ResolveData } from "@angular/router";
 import { UsuarioService } from "../usuario.service";
 
 export const resolveUsuario: ResolveData = (activeRoute: ActivatedRouteSnapshot) => {
-  // tratar
+  // tratar direito e notificar (toaster ou popup) falha no carregamento!!
   return inject(UsuarioService).obter(activeRoute.params['idUsuario']);
 }
