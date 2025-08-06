aa6f6bc21ce575bd24e6a935963173be386296fd
adrianonobre <adrianonobre@mpf.mp.br>
2025-08-06 14:51:39 -0300

    Aula 02 - parte 1

--- src/app/layout-header/layout-header.ts
+++ src/app/layout-header/layout-header.ts
@@ -3,9 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'header [app-layout-header]',
   imports: [],
+  host: {
+    '[class.header-gigante]' : 'gigante',
+    '(click)' : 'headerClick($event)'
+  },
   templateUrl: './layout-header.html',
   styleUrl: './layout-header.scss'
 })
 export class LayoutHeader {
+
+  protected titulo = 'Treinamento Angular Conceitos';
+
+  protected logoGradiente = false;
+
+  protected tituloCentralizado = false;
+
+  protected opacidade = 1;
+
+  protected gigante = false;
+
+
+  protected headerClick(event: MouseEvent) {
+    if (event.ctrlKey) {
+      this.gigante = !this.gigante;
+    }
+  }
+
+  protected logoClick(event: MouseEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.logoGradiente = !this.logoGradiente;
+  }
 
 }
