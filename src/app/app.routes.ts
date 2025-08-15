4c5c2c3e2533ab1e3efff49c1b3f8d4eb03a3ef4
adrianonobre <adrianonobre@mpf.mp.br>
2025-08-15 15:23:15 -0300

    Aula 05 - Criação das rotas iniciais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { HomePortal } from './home-portal/home-portal';
+import { TwoWayPai } from './exemplos/two-way/two-way-pai/two-way-pai';
+import { UsuariosListagem } from './exemplos/usuarios/usuarios-listagem/usuarios-listagem';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    title: 'Home - Treinamento Conceitos',
+    component: HomePortal
+  },
+  {
+    path: 'exemplos',
+    children: [
+      {
+        path: 'two-way',
+        title: 'Two-Way Binding',
+        component: TwoWayPai
+      },
+      {
+        path: 'usuarios',
+        title: 'Usuários',
+        component: UsuariosListagem
+      }
+    ]
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,42 +1,18 @@
 import { Component } from '@angular/core';
-import { TwoWayPai } from './exemplos/two-way/two-way-pai/two-way-pai';
-import { HomePortal } from './home-portal/home-portal';
 import { Layout } from './layout/layout';
-import { UsuariosListagem } from './exemplos/usuarios/usuarios-listagem/usuarios-listagem';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [
-    Layout,
-    HomePortal,
-    TwoWayPai,
-    UsuariosListagem
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    Layout
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-
-  protected tela: 'home' | 'two-way' | 'usuarios' = 'usuarios';
-
-  // private elementoMain = viewChild<ElementRef>('appMain');
-
-  // private header = viewChild.required(LayoutHeader);
-
-  // private botoes = viewChildren<ElementRef>('botao');
-
-
-  // ngOnInit() {
-  //   this.elementoMain()?.nativeElement.classList.add('icones-grandes');
-
-  //   const header = this.header();
-  //   if (header) {
-  //     header.gigante = true;
-  //     header.tituloCentralizado = true;
-  //   }
-
-  //   console.log(this.botoes().map(b => b.nativeElement.innerText));
-
-  // }
 
 }
