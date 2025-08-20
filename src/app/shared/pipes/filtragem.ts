620a09b8ba22331eb041cb9421afbff208edab09
adrianonobre <adrianonobre@mpf.mp.br>
2025-08-20 11:24:53 -0300

    Aula 07 - Preparação

--- src/app/shared/pipes/filtragem.ts
+++ src/app/shared/pipes/filtragem.ts
@@ -1,7 +1,7 @@
-export function obterItensFiltrados<T>(itens: T[], texto?: string | null) {
+export function obterItensFiltrados<T>(itens: readonly T[], texto?: string | null) {
   if (!texto)
     return itens;
 
   texto = texto.toLowerCase();
-  return itens.filter(a => JSON.stringify(a).toLowerCase().includes(texto));
+  return itens.filter(a => Object.values(a as object).join('#').toLowerCase().includes(texto));
 }
