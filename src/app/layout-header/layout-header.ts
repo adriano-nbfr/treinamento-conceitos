7de6b9d15fe9f6353e6d3fb7cb372dc6ab551efe
adrianonobre <adrianonobre@mpf.mp.br>
2025-10-08 15:36:14 -0300

    Aula 02 - Binding e Event Listener

--- src/app/layout-header/layout-header.ts
+++ src/app/layout-header/layout-header.ts
@@ -3,9 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'header[app-layout-header]',
   imports: [],
+  host: {
+    'class' : 'app-header',
+    '[class.header-gigante]' : 'gigante',
+    '(click)' : 'headerClick($event)'
+  },
   templateUrl: './layout-header.html',
   styleUrl: './layout-header.scss'
 })
 export class LayoutHeader {
+
+  protected titulo = 'Treinamento Angular';
+
+  protected logoGradiente = false;
+
+  protected tituloCentralizado = false;
+
+  protected opacidadeTitulo = 0.8;
+
+  protected gigante = false;
+
+
+  protected headerClick(event: MouseEvent) {
+    if (event.ctrlKey) {
+      this.gigante = !this.gigante;
+    }
+  }
+
+  protected logoClick(event: MouseEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.logoGradiente = !this.logoGradiente;
+  }
 
 }
