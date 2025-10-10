0276f66dd608776205283d96bdd2c0f92cbb6112
adrianonobre <adrianonobre@mpf.mp.br>
2025-10-10 01:29:40 -0300

    Aula 02 - Exercício  de output para rebaixar destaques

--- src/app/home-portal/portal-destaques/portal-destaques.ts
+++ src/app/home-portal/portal-destaques/portal-destaques.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { AtalhoSistema } from '../atalho-sistema';
 
 @Component({
@@ -11,5 +11,18 @@ export class PortalDestaques {
 
   /** Um array com dados de atalhos a exibir como links de destaque no portal */
   atalhos = input.required<AtalhoSistema[]>();
+
+  /** Evento disparado ao clicar em um atalho durante a edição, indicando que ele deve deixar os destaques. */
+  atalhoRebaixado = output<AtalhoSistema>();
+
+  editando = false;
+
+
+  protected atalhoClick(atalho: AtalhoSistema, event: MouseEvent) {
+    if (this.editando) {
+      event.preventDefault(); // evita que o link seja acionado
+      this.atalhoRebaixado.emit(atalho);
+    }
+  }
 
 }
